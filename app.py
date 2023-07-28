0e6a3df020a2d835d01a97d944f0a5dc8046629e
catspyder <32828243+catspyder@users.noreply.github.com>
2023-07-28 10:48:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import googleapiclient.errors
 import json
 import pymongo 
 import pandas as pd
-from sqlalchemy import create_engine
+from SQLAlchemy import create_engine
 
 # import pandas as pd
 # from mysql import connector
-con=create_engine(url='postgres://catspyder:Q5SWng1mEdtp@ep-hidden-brook-76474253.us-east-2.aws.neon.tech/neondb')
+con=create_engine(url='postgresql://catspyder:Q5SWng1mEdtp@ep-hidden-brook-76474253.us-east-2.aws.neon.tech/neondb')
 
 
 class Youtube:
